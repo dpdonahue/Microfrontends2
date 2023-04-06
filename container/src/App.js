24479b366d19e046b1f32eace381f574bd51e348
David P. Donahue <dpdonahue@gmail.com>
2023-04-06 13:53:08 -0400

    Initial commit

---
+++ container/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+    return (
+        <>
+            <h1>Hello, Cruel World!</h1>
+            <hr />
+            <MarketingApp />
+        </>
+    );
+};
