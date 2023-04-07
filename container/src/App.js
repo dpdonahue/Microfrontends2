2f6d731f594cefc11a04433f673629a9dc5b7cd2
David P. Donahue <dpdonahue@gmail.com>
2023-04-07 09:54:58 -0400

    Added header

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
 import MarketingApp from "./components/MarketingApp";
+import Header from './components/Header';
 
 export default () => {
     return (
-        <>
-            <h1>Hello, Cruel World!</h1>
-            <hr />
-            <MarketingApp />
-        </>
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
     );
 };
